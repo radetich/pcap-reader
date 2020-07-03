c5e25a9723c441e1caec19ca47afea6c38cd4253
Cole Radetich <56006689+radetich@users.noreply.github.com>
2020-07-02 20:34:21 -0600

    Add files via upload
    trimmed extra dev files from previous project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pcap_reader
 An open-sourced program to read vCan data.
+
+** Documented and Formatted by Cole Radetich
+** Written/Sourced by Nickolaas Bender
+
+
+## LETS GET THIS PARTY ON DA ROAD ##
+
+
+Notes here, Build instructions
+
---
+++ include/pcap_reader.hpp
@@ -0,0 +1,15 @@
+#ifndef PCAP_READER_HPP
+#define PCAP_READER_HPP
+
+#include <string>
+#include <iostream>
+#include <pcap.h>
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+
+using namespace boost::filesystem;
+using namespace std;
+
+vector <string> get_pcaps(string folder_path);
+
+#endif
